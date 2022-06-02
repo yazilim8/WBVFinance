8cd9e1b2aa8b242fd26ed8cf2eabbc5c1a41cf2b
Uğur <yazilim8.wbventures@gmail.com>
2022-06-02 13:53:08 +0300

    referrerPolicy fixed

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
           href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css'
           integrity='sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=='
           crossOrigin='anonymous'
-          referrerpolicy='no-referrer'
+          referrerPolicy='no-referrer'
         />
         <title>WBV FINANCE</title>
       </Head>
